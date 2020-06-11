ec1d02982a372b5566a6965825bde156613c26fe
burakturkmen <burakturkmen96@gmail.com>
2020-06-11 18:28:25 +0200

    Burak Türkmen / 13.06.2020 While/Do-While Mentoring

---
+++ src/Burak/While_DoWhile/Do_While_Loops/Do_While_Loops_2.java
@@ -0,0 +1,21 @@
+package Burak.While_DoWhile.Do_While_Loops;
+
+public class Do_While_Loops_2 {
+    public static void main(String[] args) {
+        //TODO---------- DO-WHILE
+
+        int a = 10;
+
+        do {
+
+            System.out.println("a = " + a);
+
+            a--;
+
+        }
+
+        while (a > 0);
+
+
+    }
+}
---
+++ src/Burak/While_DoWhile/WhileLoops/a_while.java
@@ -0,0 +1,31 @@
+package Burak.While_DoWhile.WhileLoops;
+
+public class a_while {
+    public static void main(String[] args) {
+
+        /*
+            döngü değişkeni
+
+            while(koşul){
+
+            işlem1
+            işlem2
+            işlem3
+
+            arttırma, azaltma
+
+            }
+         */
+
+        int sayi = 0;
+
+
+        while (sayi < 10){
+
+            sayi++;
+
+
+            System.out.println("LOOP");
+        }
+    }
+}
