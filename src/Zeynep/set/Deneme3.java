0f859c4561523b0f482478d5dae7d581ca1f868f
AKMERCAN <z.ucar@windowslive.com>
2020-07-08 00:07:34 -0400

    nested loop and set

---
+++ src/Zeynep/set/Deneme3.java
@@ -0,0 +1,16 @@
+package Zeynep.set;
+
+public class Deneme3 {
+
+    /*
+
+    Oncelikle icinde degerleri olan bir array i arraylist e cevirecek bir metod yaziniz.
+
+    {"blau", "grun" ,"schwarz", "weiss", "gelb", "braun"}
+
+    return tipi arrayList olacak
+
+    main metod altinda metod ile gelen listeyi kullanarak HashSet, TreeSet ve linkedHashSet leri doldurun.
+
+    */
+}
