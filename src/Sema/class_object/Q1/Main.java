a9dbbb7ab04810e10dcdbadd966185b84cde1d4c
Sema <semayartasi@hotmail.com>
2020-07-10 17:26:00 -0400

    Class&Object

---
+++ src/Sema/class_object/Q1/Main.java
@@ -0,0 +1,17 @@
+package Sema.class_object.Q1;
+class Test{
+    int i;
+}
+
+class Main {
+    public static void main(String[] args) {
+      //  Test t;
+      // System.out.println(t.i);
+    }
+}
+/*
+0
+garbage value
+compiler error
+runtime error
+ */
