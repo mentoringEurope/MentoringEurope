b18f012770efe38f6cee80fc4d2b16717b97f158
AKMERCAN <z.ucar@windowslive.com>
2020-07-17 12:51:59 -0400

    accessModifier

---
+++ src/Zeynep/acc_modifier2/BaskaPackageSinifi.java
@@ -0,0 +1,17 @@
+package Zeynep.acc_modifier2;
+
+import Zeynep.accessModifier.NeNedir;
+
+public class BaskaPackageSinifi {
+
+    public static void main(String[] args) {
+
+        NeNedir paketDisi = new NeNedir();
+
+        paketDisi.yas = 7;
+        paketDisi.yiliYazdir();
+
+
+
+    }
+}
