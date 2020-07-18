f2332e37483f1c33b0ce3284cdd83dda8cb9be0b
burakturkmen <burakturkmen96@gmail.com>
2020-07-18 11:08:09 +0200

    Burak Türkmen - 18/07/2020 - Non-Access Modifiers

---
+++ src/Burak/Classes/MultipleClasses/odev.java
@@ -0,0 +1,6 @@
+package Burak.Classes.MultipleClasses;
+
+public class odev {
+
+    //todo       Karnıyarık örneği gibi kendiniz bir konu bulup onu Classlar arasında çağırın. Örn: Hayvan, insan, araba, bilgisayar...
+}
