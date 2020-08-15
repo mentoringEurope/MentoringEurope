5a42a09e8a176a2b224ab5e01e7cf140777c2d04
burakturkmen <burakturkmen96@gmail.com>
2020-08-15 12:26:31 +0200

    Burak Türkmen - 15/08/2020 - Interface

---
+++ src/Burak/Interface/Ornek1/calisan.java
@@ -0,0 +1,9 @@
+package Burak.Interface.Ornek1;
+
+public class calisan implements IDatabase {
+
+    @Override
+    public void hesapAcma() {
+        System.out.println("Bir Çalışan datası eklendi...");
+    }
+}
