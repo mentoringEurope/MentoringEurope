0c1aa294f3702e4e8089c6ea2b82441432fdb21c
burakturkmen <burakturkmen96@gmail.com>
2020-08-22 10:56:04 +0200

    Burak Türkmen - 22/08/2020 - Abstraction

---
+++ src/Burak/Abstraction/DataBaseOrnek/samsungDatabase.java
@@ -0,0 +1,15 @@
+package Burak.Abstraction.DataBaseOrnek;
+
+public class samsungDatabase extends abstractDatabase{
+
+
+    @Override
+    void get() {
+        System.out.println("Samsung çalışan bilgisi elde etme...");
+    }
+
+    @Override
+    void update() {
+        System.out.println("Samsung çalışan bilgisi güncelleme...");
+    }
+}
